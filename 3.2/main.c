6b49ab568baa0c74d06b16dc11ddac505a0c2d41
Moisa Oana <oana_moisa29@yahoo.com>
2020-01-05 21:44:37 +0200

    first_commit

--- 3.2/main.c
+++ 3.2/main.c
@@ -9,16 +9,35 @@ int hermiteRecursive(int n,int x)
     if(n==1)
         return 2*x;
     else
-        return 2*n*hermiteRecursive(n-1,x)-2*(n-1)*hermiteRecursive(n-1,x);
+        return 2*n*hermiteRecursive(n-1,x)-2*(n-1)*hermiteRecursive(n-2,x);
 }
+int hermiteNonRecursive(int n, int x)
+{
+    if(n==0)
+        return 1;
+    int a1=1,a2=2*x,a=0;
+    if(n==1)
+        return a2;
+    for(int i=2;i<=n;i++)
+    {
+        a=2*i*a2-2*(i-1)*a1;
+        a1=a2;
+        a2=a;
+    }
+    return a;
 
+}
 int main() {
     int n,x;
     printf("n= ");
     scanf("%d",&n);
     printf("x= ");
     scanf("%d",&x);
+    printf("Recursive solution: \n");
     printf("H%d(%d)=", n,x);
     printf("%d",hermiteRecursive(n,x));
+    printf("\nNon-Recursive solution: \n");
+    printf("H%d(%d)=", n,x);
+    printf("%d",hermiteNonRecursive(n,x));
     return 0;
 }
